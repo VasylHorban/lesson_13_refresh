b1f02ed2ad1d53673d42c9eb8b9174b48865a7e5
VasylHorban <nagatolt427@gmail.com>
2020-09-02 15:33:23 +0300

    'second'

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const formModule = (function () {
     const emailRegExp = /^[a-zA-Z0-9_#&\.\+]{1,}@[a-zA-Z0-9_#&\.\+]{1,}\.com|ua|ru|org|pl$/
     const passwordRegExp = /^\w{8,16}$/
     const imgs = ['https://image.flaticon.com/icons/svg/929/929416.svg', 'https://image.flaticon.com/icons/svg/929/929440.svg']
+    const alertText = ['name', 'surname', 'email address', 'password']
     let inputElem;
     let agreeCheckElem;
 
@@ -19,6 +20,7 @@ const formModule = (function () {
         }
         agreeCheckElem.addEventListener('change', switchBtn)
         getS('#btn-submit').addEventListener('click', submit)
+        getS('#btn-explore').addEventListener('click', explore)
     }
     function switchBtn(){
         if(agreeCheckElem.checked){
@@ -27,9 +29,47 @@ const formModule = (function () {
             getS('#btn-submit').disabled = true
         }
     }
+    function showAlert(elem){
+        let span = elem.nextSibling.nextSibling;
+        let idList = {
+            name : 0,
+            surname : 1,
+            email : 2,
+            password : 3
+        }
+        span.textContent = 'Please provide a valid ' + alertText[idList[elem.id]]
+        span.style.display = 'block'
+        
+    }
+    function hideAlert(elem){
+        elem.nextSibling.nextSibling.style.display = 'none'
+    }
+    function explore(){
+        getS('#sign-up').classList.remove('dark')
+        getS('body').style.background = '#fbfbfb';
+        getS('.succsess-window').style.display = 'none'
+        getS('.succsess-window').style.top = '5%'
+        clear()
+
+
+    }
+    
     function submit(){
         console.log('submit')
+        getS('#sign-up').classList.add('dark')
+        getS('body').style.background = 'rgba(85, 74, 74, 0.42)'
+        getS('.succsess-window').style.display = 'block'
+        getS('.succsess-window').style.top = '100px;'
     }
+    function clear(){
+        for(let elem of inputElem){
+            elem.value = '';
+            elem.nextSibling.style.background = 'none'
+            elem.style.borderColor = 'grey'
+        }
+        agreeCheckElem.checked = false
+    }
+    
     function checkValid() {
         let bool;
         if (this.id == 'name' || this.id == 'surname') {
@@ -41,9 +81,11 @@ const formModule = (function () {
         }
         if (bool) {
             showIcon(this, true)
+            hideAlert(this);
             this.valid = true
         } else {
             showIcon(this, false)
+            showAlert(this);
             this.valid = false
         }
         checkAllData();
